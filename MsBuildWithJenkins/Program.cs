289a213f2f05ec18a42c0f683f82717eba98e113
melon1313 <10212130@gm.nttu.edu.tw>
2021-07-08 16:25:55 +0800

    加入專案檔案。

---
+++ MsBuildWithJenkins/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsBuildWithJenkins
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello Alice!");
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsBuildWithJenkins;
+using System;
+using System.IO;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string expected = "Hello Alice!";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(expected, result);
+            }
+        }
+    }
+}
