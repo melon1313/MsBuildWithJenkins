00a180a49b3a8870773dc90ffe1e8c81bf0705fe
melon1313 <10212130@gm.nttu.edu.tw>
2021-07-09 17:23:03 +0800

    test new test project

---
+++ TestProject2/UnitTest1.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace TestProject2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.True(true);
+        }
+    }
+}
